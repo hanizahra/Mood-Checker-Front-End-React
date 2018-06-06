7d3445c0e07b8bf51114522ce83dc7b0e0f10deb
hanizahra <hanizahra@gmail.com>
2018-06-06 11:47:04 -0400

    cleaned

--- src/Services.js
+++ src/Services.js
@@ -15,7 +15,6 @@ const Services = {
 			data: 'text= ' + userInput,
 			headers: {
 			'Access-Control-Allow-Origin' : '*',
-			// 'Api-Key' : '20d22b6a-7c2b-49fa-ab4e-08dbb65b6619',
 			'Content-Type' : 'application/x-www-form-urlencoded' 
 			}
 		})
